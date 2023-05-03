22ad2a462bc7768fb390e1bc187729ee996832fb
Ram <panigrahyr8@gmail.com>
2023-05-03 18:55:25 +0200

    Initial

--- django_fastapi/api/models.py
+++ django_fastapi/api/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Item(models.Model):
     title = models.CharField(max_length=50)
-    description = models.TextField()
+    description =  models.TextField()
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="items"
     )
